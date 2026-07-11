e2ca17314fbea87db6fdec93f5aa4cafce8470ee
jp200883-sudo <jp200883@gmail.com>
2026-07-11 13:21:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware  # ✅ YEH ADD KARO!
+from fastapi import FastAPI, Request  # ✅ YEH ADD KARO!
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from contextlib import asynccontextmanager
