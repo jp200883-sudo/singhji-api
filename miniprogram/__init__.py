e82da55b7b3bfbc1f8caf9a6fbf69755f0260864
jp200883-sudo <jp200883@gmail.com>
2026-07-11 12:09:38 +0530

    Delete miniprogram directory

--- miniprogram/__init__.py
+++
@@ -1,8 +0,0 @@
-"""🦁 Singh Ji AI Mini-Program Platform"""
-from .auth import MiniAuth
-from .payment import MiniPayment
-from .storage import MiniStorage
-from .sandbox import SandboxRunner
-from .developer import DeveloperPortal
-
-__version__ = "1.0.0"
